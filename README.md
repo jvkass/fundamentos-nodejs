50684ab841f250966ed792452d451657b97b2ba9
jvkass <jvcnmata@gmail.com>
2021-10-06 15:26:33 -0300

    Utilizando os Métodos HTTP

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fundamentos-nodejs
 Configuração base do projeto ignite modulo I
+
+## Metodos utilizados na aula
+
+GET - Buscar uma informação dentro do servidor
+POST - Inserir uma informação no servidor 
+PUT - Alterar uma informação no servidor 
+PATCH - Alterar uma informação especifica
+DELETE - Deletar uma informação no servidor
+
--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response)=>{
-    return response.send({message:"hello world Ignite"});
+app.get("/courses", (request, response)=>{
+    return response.json(["Cursos 1", "Cursos 2", "Cursos 3"]);
+});
+
+app.post("/courses", (request, response)=>{
+    return response.json(["Cursos 1", "Cursos 2", "Cursos 3","Cursos 4"]);
+});
+
+app.put("/courses:id",(request, response)=>{
+    return response.json(["Cursos 6", "Cursos 2", "Cursos 3","Cursos 4"]);
+});
+
+app.patch("/courses:id",(request, response)=>{
+    return response.json(["Cursos 6", "Cursos 7", "Cursos 3","Cursos 4"]);
+});
+
+app.delete("/courses:id",(request, response)=>{
+    return response.json(["Cursos 6", "Cursos 7","Cursos 4"]);
 });
 
 app.listen(3333);
