2a67c8d3d2c8f6385edcc7b597da28572ea4b256
jvkass <jvcnmata@gmail.com>
2021-10-06 16:14:22 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -2,23 +2,32 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response)=>{
+    const query = request.query;
+    console.log(query);
     return response.json(["Cursos 1", "Cursos 2", "Cursos 3"]);
 });
 
 app.post("/courses", (request, response)=>{
+    const body = request.body;
+    console.log(body);
+
     return response.json(["Cursos 1", "Cursos 2", "Cursos 3","Cursos 4"]);
 });
 
-app.put("/courses:id",(request, response)=>{
+app.put("/courses/:id",(request, response)=>{
+    const {id} = request.params;
+    console.log(id);
     return response.json(["Cursos 6", "Cursos 2", "Cursos 3","Cursos 4"]);
 });
 
-app.patch("/courses:id",(request, response)=>{
+app.patch("/courses/:id",(request, response)=>{
     return response.json(["Cursos 6", "Cursos 7", "Cursos 3","Cursos 4"]);
 });
 
-app.delete("/courses:id",(request, response)=>{
+app.delete("/courses/:id",(request, response)=>{
     return response.json(["Cursos 6", "Cursos 7","Cursos 4"]);
 });
 
